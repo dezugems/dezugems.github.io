9d1ec7a4e2aef4e391fecc351ef27befec77dd21
Stefano <michste93@gmail.com>
2015-10-22 09:28:44 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($){
+  		 
+	$('.cd-primary-nav').on('click', function(event){
+		if($(event.target).is('.cd-primary-nav')) 
+		   $(this).children('ul').toggleClass('is-visible');
+		   
+	});
+});
+
